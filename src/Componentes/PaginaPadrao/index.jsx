c6a7485299a2285fe9b7a9e33e3f65846a569db7
Sabrina Andrade <eusabrinaps@icloud.com>
2025-09-25 10:40:30 -0300

    Componente Banner

--- src/Componentes/PaginaPadrao/index.jsx
+++ src/Componentes/PaginaPadrao/index.jsx
@@ -1,4 +1,4 @@
-import Banner from "@/componentes/Banner";
+import Banner from "@/Componentes/Banner";
 import { Outlet } from "react-router-dom";
 export default function PaginaPadrao() {
     return (
